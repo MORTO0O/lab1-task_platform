c071370aa743c74dd2356b51cfbcc1d344b023e0
Егор Корнеев <eakorneev@mai.education>
2026-03-16 12:47:46 +0300

    fixed file_source output

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from .sources.API_source import APITaskSource
 if __name__ == '__main__':
     receiver = TaskReceiver()
 
-    receiver.add_source(FileTaskSource("data/tasks.json"))
-    receiver.add_source(GeneratorTaskSource(count=5))
+    receiver.add_source(FileTaskSource("src/tasks.json"))
+    receiver.add_source(GeneratorTaskSource(count=8))
     receiver.add_source(APITaskSource())
 
     print("Collecting tasks from all sources...\n")
 
     for task in receiver.stream_tasks():
-        print(f"done: Task {task.id} | payload {task.payload}")
+        print(f"done: Task {task.id}; payload {task.payload}")
 
     print(f"\nNumber of tasks: {len(receiver.collect_tasks())}")
